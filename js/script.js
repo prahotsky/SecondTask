8c0bc2bdcc6245eae1ed2fb1d44e933b59f2fa51
Максец <prahotsky96@gmail.com>
2018-10-03 00:03:47 +0300

    second task

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
     $.get( "https://api.github.com/repos/thomasdavis/backbonetutorials/contributors", function( data ) {
         data.forEach(function(item) {
-            console.log(item);
+            showItems(item);
         });
     });
 });
+
+function showItems(item) {
+
+    let $avatar = $("<div></div>").addClass("avatar");
+    let $login = $("<p></p>").html(item.login);
+    let $contributions = $("<p></p>").html("Contributions: " + item.contributions);
+    let $caption = $("<div></div>")
+        .addClass("caption")
+        .append($login)
+        .append($contributions);
+    let $contactCard = $("<div></div>")
+        .addClass("main-item")
+        .append($avatar)
+        .append($caption);
+    $avatar.css("background", "url(" + item.avatar_url + ") no-repeat").css("background-size", "contain");
+    $(".main-item-wrapper").append($contactCard);
+}
+
 
